aa2a4aafcbb70b37393f1a31a2ddf46f5ab52a46
CatBook2010 <vkhaaneev@gmail.com>
2023-04-28 16:42:59 -0400

    commit28.04.2023

--- source/map.py
+++ source/map.py
@@ -4,16 +4,36 @@ from wall import Wall
 
 class Map:
     def __init__(self, map_def: list):
+        self.images = {"berry":pygame.image.load("images/berry.png"),
+                       "berry_2":pygame.image.load("images/berry_2.png"),
+                       "wall":pygame.image.load("images/wall_blue.png")
+                       }
+        
+        for key in self.images:
+            self.images[key] = pygame.transform.scale(self.images[key], (20, 20))
+            
+
         self.parse(map_def)
 
     def parse(self, map_def: list):
         x = 0
         y = 0
-        map_x = 0
-        map_y = 0
+
+        objects = []
 
         for row in map_def:
             row_list = row.split("")
             for symbol in row_list:
                 if symbol == "#":
-                    Wall()
+                    objects.append(Wall(x, y))
+                elif symbol == "0":
+                    objects.append(Berry(x, y, False))
+                elif symbol == "O":
+                    objects.append(Berry(x, y, True))
+                x += 1
+            y += 1
+        
+        self.objects = objects
+    
+    def render(self, screen):
+        screen.blit()
